8239f230a470c7ae1ea16c8a5d3b72039765e0c9
Luana Barremaker <luanabruschel@gmail.com>
2024-05-03 16:48:01 -0300

    icons dark and light mode

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // Função para dark mode
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
+
+     // Verifica se o modo escuro está ativado e adiciona/remove a classe ao botão conforme necessário
+     var darkModeEnabled = document.body.classList.contains('dark-mode');
+     var navbarToggler = document.getElementById('dark-mode-toggler');
+     if (navbarToggler) {
+         if (darkModeEnabled) {
+             navbarToggler.classList.add('dark-mode');
+         } else {
+             navbarToggler.classList.remove('dark-mode');
+         }
+     }
 }
